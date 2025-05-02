b3de770510d36309ebd890ae7224af4e26e85d78
Nouman Gul <nouman@oasislivestock.com>
2025-05-02 11:09:26 +0400

    Initial Commit

---
+++ SampleCleanArchitecture/SampleCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCleanArchitectureApp.swift
+//  SampleCleanArchitecture
+//
+//  Created by Rohit Kumar on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
